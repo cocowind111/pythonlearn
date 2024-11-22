08b6a5d2c45300d15f442e2cae176350280bd5c0
cocowind111 <825871601@qq.com>
2024-11-22 18:20:50 +0800

    test1

--- test1.py
+++
@@ -1,8 +0,0 @@
-class gagadenvyou:
-    def __init__(self,name,age):
-        self.name= name 
-        self.age = age 
-    def __str__(self):
-        return f"{self.name},{self.age}"
-b=gagadenvyou('yeye',18)
-print(b.name)
