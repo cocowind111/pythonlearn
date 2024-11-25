66488022a24ba2afbf4d45563a5d00dee2a93d92
cocowind111 <825871601@qq.com>
2024-11-25 18:17:53 +0800

    12

--- classimport.py
+++ classimport.py
@@ -1,7 +1,10 @@
 from classdef import gagadenvyou
-
+import classdef
 gaga= gagadenvyou('yeye',18)
+gaga1=classdef.gagadenvyou('yeye',18)
 if __name__=="__main__": #只有这个py文件作为主程序执行时，才会调用这个if语句下面的代码。如果是模块被调用的话则不会执行。
     print(gaga)
     print(gaga.name)
+    print(gaga1)
+    print(gaga1.name)
 
---
+++ classvariable.py
@@ -0,0 +1,17 @@
+class variable:
+    name="a"
+    var_type="类变量"
+    def __init__(self):
+        self.value=18
+    def shilivar(self,shili):
+        self.shili="shilivariable"
+    def jubuvar(self,jubu):
+        money=jubu*100
+        print(money)
+        return money
+b=variable()
+c=b.jubuvar(0.1)
+
+    
+    
+
